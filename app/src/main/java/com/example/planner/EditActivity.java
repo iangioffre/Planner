423a0ad4a741c17a586a66b8ab7189a8faa7e178
Kelsey <kelseylally1@gmail.com>
2019-12-04 13:53:21 -0800

    inital code for edit activity

--- app/src/main/java/com/example/planner/EditActivity.java
+++ app/src/main/java/com/example/planner/EditActivity.java
@@ -2,7 +2,11 @@ package com.example.planner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return true;
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
