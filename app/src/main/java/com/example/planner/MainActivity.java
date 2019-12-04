74fd798adf2a5adc0e2650b31746868b5f23022d
Ian Gioffre <iangioffre@gmail.com>
2019-12-03 16:27:20 -0800

    implemented sort menu options

--- app/src/main/java/com/example/planner/MainActivity.java
+++ app/src/main/java/com/example/planner/MainActivity.java
@@ -1,58 +1,72 @@
 package com.example.planner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
     static final String TAG = "MainActivityTag";
+
+    EventOpenHelper openHelper;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ListView listView = new ListView(this);
+        listView = new ListView(this);
         setContentView(listView);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // open the edit activity
-//                Intent intent = new Intent(MainActivity.this, EditActivity.class);
-//                intent.putExtra("id", id);
-//                startActivity(intent);
+//        Intent intent = new Intent(this, EditItemActivity.class);
+//        intent.putExtra("id", id);
+//        startActivity(intent);
             }
         });
 
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                // allow a deletion
+                // allow a deletion using AlertDialog
 
                 return true;
             }
         });
 
-        final EventOpenHelper openHelper = new EventOpenHelper(this);
+        openHelper = new EventOpenHelper(this);
+
         Event event = new Event();
         event.setTitle("TITLE2");
         event.setDateTime("2019-12-22 12:00:00");
         openHelper.insertMeeting(event);
 
+        sortListByDate();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // update the listview with the db
+    }
+
+    private void sortListByDate() {
         Cursor cursor = openHelper.getSelectAllEventsByDateCursor();
         SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(
                 this,
@@ -84,10 +98,101 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(cursorAdapter);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private void sortListByClass() {
+        Cursor cursor = openHelper.getSelectAllEventsByDateCursor();
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                cursor,
+                new String[] {EventOpenHelper.TITLE},
+                new int[] {android.R.id.text1},
+                0
+        ) {
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                TextView tv1 = view.findViewById(android.R.id.text1);
+                TextView tv2 = view.findViewById(android.R.id.text2);
 
-        // update the listview with the db
+                String title = cursor.getString(cursor.getColumnIndex(openHelper.TITLE));
+                String course = cursor.getString(cursor.getColumnIndex(openHelper.COURSE));
+
+                tv1.setText(title);
+                tv2.setText(course);
+            }
+
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup parent) {
+                LayoutInflater cursorInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                return cursorInflater.inflate(android.R.layout.simple_list_item_2, parent, false);
+            }
+        };
+
+        listView.setAdapter(cursorAdapter);
+    }
+
+    private void sortListByImportance() {
+        Cursor cursor = openHelper.getSelectAllEventsByDateCursor();
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                cursor,
+                new String[] {EventOpenHelper.TITLE},
+                new int[] {android.R.id.text1},
+                0
+        ) {
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                TextView tv1 = view.findViewById(android.R.id.text1);
+                TextView tv2 = view.findViewById(android.R.id.text2);
+
+                String title = cursor.getString(cursor.getColumnIndex(openHelper.TITLE));
+                String priority = cursor.getString(cursor.getColumnIndex(openHelper.PRIORITY));
+
+                tv1.setText(title);
+                tv2.setText(priority);
+            }
+
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup parent) {
+                LayoutInflater cursorInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                return cursorInflater.inflate(android.R.layout.simple_list_item_2, parent, false);
+            }
+        };
+
+        listView.setAdapter(cursorAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.addMenuItem:
+//                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+//                startActivity(intent);
+                return true;
+            // task: finish the two other cases, show toast messages
+            case R.id.classListMenuItem:
+                // show class list
+                return true;
+            case R.id.sortByClassMenuItem:
+                sortListByClass();
+                return true;
+            case R.id.sortByDateMenuItem:
+                sortListByDate();
+                return true;
+            case R.id.sortByImportanceMenuItem:
+                sortListByImportance();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
